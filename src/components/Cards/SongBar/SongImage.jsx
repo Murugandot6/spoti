c8fc53fe1b404912334be741213b3733ad65d1b3
[dyad] <mskdot6@gmail.com>
2025-08-10 17:06:55 +0530

    [dyad] wrote 1 file(s)

--- src/components/Cards/SongBar/SongImage.jsx
+++ src/components/Cards/SongBar/SongImage.jsx
@@ -1,5 +1,5 @@
 import PlayPause from "../PlayPause"
-import { songImage as defaultSongImage } from '../../assets/images'; // Import default image
+import { songImage as defaultSongImage } from "../../assets/images/index.js"; // Explicitly import from index.js
 
 const SongImage = ({ song, activeSong, isPlaying, handlePlayClick, handlePauseClick }) => {
   const imageUrl = song?.image?.[0]?.link || defaultSongImage; // Use fallback
