b8820b415eac01b8ccd14e4217e1cbd0c306e983
[dyad] <mskdot6@gmail.com>
2025-08-10 17:02:15 +0530

    [dyad] wrote 1 file(s)

--- src/components/Sidebar/Footer.jsx
+++ src/components/Sidebar/Footer.jsx
@@ -1,21 +1,16 @@
 import { FaGlobe } from "react-icons/fa"
-import { useGetCountryInfoQuery } from '../../redux/services/DeezerApi'
+// Removed import for useGetCountryInfoQuery as it's from the removed DeezerApi
 
 const Footer = () => {
-  const { data, isFetching, error } = useGetCountryInfoQuery()
+  // Removed useGetCountryInfoQuery hook as it's no longer available
+  // const { data, isFetching, error } = useGetCountryInfoQuery()
 
   return (
     <footer className="px-2 flex flex-1 flex-col justify-end gap-6 items-start">
       <button className="px-2 border border-white/5 bg-white/5 h-[34px] rounded-[20px] text-gray-300 text-xs font-semibold flex flex-row items-center justify-center gap-2">
         <FaGlobe size={15} />
         <span className="mr-1">
-          {
-            isFetching ?
-              'Loading...' :
-              error ?
-                'Error' :
-                data?.country
-          }
+          Global {/* Display static text as country info is not available */}
         </span>
       </button>
       <div className="flex flex-col gap-4">
